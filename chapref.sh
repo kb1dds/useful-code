c35fb1e9bffd6d1de856eb68a379df7e72a9739f
Michael Robinson <kb1dds@gmail.com>
2021-06-15 14:18:08 -0400

    Add files via upload

---
+++ chapref.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+# Script to identify references that are present but not defined in a given LaTeX file
+# These are likely cross-chapter references, and ought to be called out as such in the text
+
+# References found in the file
+grep -o '\\ref{[a-zA-Z:_0-9]*}' $1.tex | cut -d '{' -f 2 | cut -d'}' -f 1 | sort -u > refs.txt
+
+# Labels defined in the file
+grep -o '\\label{[a-zA-Z:_0-9]*}' $1.tex | cut -d '{' -f 2 | cut -d'}' -f 1 | sort -u > labels.txt
+
+# Want listing of every reference not defined as a label
+for ref in `diff -d refs.txt labels.txt | grep '<' | cut -d ' ' -f 2 | grep -v 'chap:'`
+do
+    grep '\label{'$ref'}' *_chap.tex | grep '\label'
+done
+
+# Clean up
+rm refs.txt
+rm labels.txt
